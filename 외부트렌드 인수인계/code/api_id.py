2edff671a5dfd4ad65878d8e5ad2a9876cb783b4
rse112 <chohy121212.naver.com>
2024-03-21 17:01:51 +0900

    퇴근~

---
+++ 외부트렌드 인수인계/code/api_id.py
@@ -0,0 +1,43 @@
+# 형섭 주임
+id_1 = {'client_id' : "caqyLeFpwxm73E1Bc4uk",
+        'client_secret' : "k9epWgz_ub"}
+
+# 윤호 선임
+id_2 = {'client_id' : "mZtQ5QquBFIrshEwfZBv",
+        'client_secret' : "jAIZheOMh8"}
+
+# 지훈 연구원
+id_3 = {'client_id' : "JBZVGpAyhfoWNwLfITGK",
+        'client_secret' : "vq_pfgUBtu"}
+
+# 범수 선임
+id_4 = {'client_id' : "eTPkCmA8Ug1GNyeu0rbD",
+        'client_secret' : "pC1iK2RSSC"}
+
+# 오미 연구원
+id_5 = {'client_id' : "uqycQbr764hs_uU2yLbw",
+        'client_secret' : "hkbDKKvm9N"}
+
+# 지우 연구원
+id_6 = {'client_id' : "3PT4BXkVGd7R67Y5hhsE",
+        'client_secret' : "fSHveYB642"}
+
+# 가현 선임
+id_7 = {'client_id' : "u30JzoczYBbu1lqwzPis",
+        'client_secret' : "ZlVh8nEuNi"}
+
+# 효경 주임
+id_8 = {'client_id' : "UcxKdYQZjj2FhdkZjgRz",
+        'client_secret' : "uy9jFsFAOr"}
+
+# 인성 연구원
+id_9 = {'client_id' : "RRYYzqMgic6itMdMQNdP",
+        'client_secret' : "6JjVKXRLfV"}
+
+# 미주 연구원
+id_10 = {'client_id' : "RqLjLk4qprxPIVBMsVB2",
+        'client_secret' : "7R5OIxUlf7"}
+
+# 이사님
+id_11 = {'client_id' : "eywsrQuPMFtlbYz1XAl7",
+        'client_secret' : "Me4ra4s8od"}
